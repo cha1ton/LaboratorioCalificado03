d1a360095dc579e1ec693df694c15ab7d4c63ce2
cha1ton <chalton.mercado@tecsup.edu.pe>
2024-11-16 15:21:50 -0500

    Initial commit

---
+++ app/src/main/java/com/mercado/chalton/laboratoriocalificado03/Apiservice.kt
@@ -0,0 +1,9 @@
+package com.mercado.chalton.laboratoriocalificado03
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("list/teacher")
+    fun getProfesores(): Call<ApiResponse>
+}
---
+++ app/src/main/java/com/mercado/chalton/laboratoriocalificado03/Profesor.kt
@@ -0,0 +1,13 @@
+package com.mercado.chalton.laboratoriocalificado03
+
+data class ApiResponse(
+    val teachers: List<Profesor>
+)
+
+data class Profesor(
+    val name: String,
+    val last_name: String,
+    val phone: String,
+    val email: String,
+    val imageUrl: String
+)
